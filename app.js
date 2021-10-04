6ef66b6cf980923c68c5e585134d4e2bc3b43034
Sherri Holmes <85898920+pholmess@users.noreply.github.com>
2021-10-04 08:56:19 -0400

    updated error codes

--- app.js
+++ app.js
@@ -9,38 +9,55 @@ const project = require('./project');
 
 const app = express();
 
-//set pug
-app.set('view engine', 'pug');
-
-//static middleware
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
-console.log("Hello World!");
-console.error("Oops!");
-
-http.get('http://localhost:3000/data.json', response => {
-    console.log(response.statusCode);
+//Home page route
+app.get('/', function(req, res, next) {
+    //log out the home route handler
+    //pass the project data into the index template
+    res.render('index', { projects });
 });
 
-//establish the root route
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-//establish the about page
-app.get('/about', (req, res) => {
+//About page route
+app.get('/about', function(req, res, next) {
     res.render('about');
 });
 
+//Project route
+app.get('/project/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = { projects }.projectId;
 
-//establish the project page
-app.get('/projects/:id', (req, res, next) => {
-    res.render('project', data);
-    next(data);
+    if (Number.isInterger(projectId) && projectId <= { projects }.length && projectId >= 0) {
+        return res.render('project', { projects });
+    } else {
+        let err = new Error("Sorry, that page does not exist.");
+        next(err);
+    }
+});
+
+//404 handler to catch undefined route reqs
+app.use((req, res, next) => {
+
+    console.log('404 error handler called');
+    res.status(404).render('not-found');
+});
+
+/* Global error handler */
+app.use((err, req, res, next) => {
+
+    if (err) {
+        console.log('Global error handler called', err);
+    }
+
+    if (err.status === 404) {
+        res.status(404).render('not-found', { err });
+    } else {
+        err.message = err.message || `Oops!  It looks like something went wrong with the server.`;
+        res.status(err.status || 500).render('error', { err });
+    }
 });
 
 
-//check to see if app is listening
+//server listen for the app
 app.listen(3000, function() {
     console.log('The app is listening on port: 3000');
 });
